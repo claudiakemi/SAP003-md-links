a02ba627bb3283aa1371498fb536198bd551b3a0
claudiakemi <claudiakemis@gmail.com>
2019-11-28 18:07:42 -0300

    trazendo array com href e text

--- cli.js
+++ cli.js
@@ -1,1 +1,8 @@
+#!/usr/bin/env node
 //este arquivo deve chamar a função mdLinks que será executada pela linha de comando
+const mdLinks = require('./lib/index.js');
+console.log('oi')
+
+mdLinks()
+  .then((result) => console.log(result))
+  .catch(console.error);
