f8908d4eb249fb688c8089a3acef66522297fccf
claudiakemi <claudiakemis@gmail.com>
2019-12-03 16:50:18 -0300

    readme

--- cli.js
+++ cli.js
@@ -3,5 +3,5 @@ const mdLinks = require('./lib/index.js');
 const path = process.argv[2];
 mdLinks(path)
 .then(secondArray => secondArray.forEach((elements) => {
-  console.log(elements.href, elements.text)}))
+  console.log(elements.href.substring(0,50), elements.text)}))
   .catch((error) => console.log(error))
