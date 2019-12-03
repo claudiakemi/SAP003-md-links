34aae1f34ec7cf047ffdfe0e4c8c6fbf1eb719f7
claudiakemi <claudiakemis@gmail.com>
2019-12-03 14:51:48 -0300

    tests ok

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-//este arquivo deve chamar a função mdLinks que será executada pela linha de comando
 const mdLinks = require('./lib/index.js');
 const path = process.argv[2];
 mdLinks(path)
-  .then((secondArray) => console.log(secondArray))
+.then(secondArray => secondArray.forEach((elements) => {
+  console.log(elements.href, elements.text)}))
   .catch((error) => console.log(error))
